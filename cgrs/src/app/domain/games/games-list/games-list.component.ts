8a602729bb9aa7e4a6baac2b884c755eecb150ca
kalitawojciech <wojtek1311997@gmail.com>
2023-05-15 23:42:57 +0200

    working on games-list

--- cgrs/src/app/domain/games/games-list/games-list.component.ts
+++ cgrs/src/app/domain/games/games-list/games-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { GameInfoResponse, GamesService } from 'src/app/core/services/api.service';
 
 @Component({
   selector: 'app-games-list',
   templateUrl: './games-list.component.html',
   styleUrls: ['./games-list.component.scss']
 })
-export class GamesListComponent implements OnInit {
+export class GamesListComponent implements OnInit, OnDestroy {
+  games: GameInfoResponse[] = [];
 
-  constructor() { }
+  private unsubscribe$ = new Subject<void>();
+
+  constructor(
+    private gamesService: GamesService
+  ) { }
 
   ngOnInit(): void {
+    this.gamesService
+      .getGames(true, null)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(games => this.games = games);
+  }
+  
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 }
